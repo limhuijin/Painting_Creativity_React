d596813df9fd15eaf49e3f9341d2ce2087927b6f
HeoSeonJin <sonjin54@naver.com>
2024-08-25 21:15:11 +0900

    css 구조 변경, 소개하기 페이지 작성(소개 사진 및 버튼, 소개 글 구현)

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,14 +1,28 @@
 import React from 'react';
-import './Footer.css'; // CSS 파일을 임포트
+import styled from 'styled-components'; 
+
+// styled-components로 footer 스타일 정의
+const StyledFooter = styled.footer`
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+  background-color: #f8f9fa; /* 원하는 배경색으로 변경 가능 */
+  z-index: 1000; /* 푸터가 다른 콘텐츠 위에 나타나게 함 */
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 1rem;
+`;
 
 function Footer() {
   return (
-    <footer className="footer d-flex justify-content-between align-items-center p-3">
+    <StyledFooter>
       <span>푸터</span>
       <div>
         {/* 여기에 푸터 내용 추가 */}
       </div>
-    </footer>
+    </StyledFooter>
   );
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,28 +1,68 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import './Header.css';  // CSS 파일을 임포트하여 스타일 적용
+import styled from 'styled-components'; // styled-components 임포트
+
+// styled-components로 스타일 정의
+const HeaderContainer = styled.header`
+  background-color: #fff;
+  padding: 20px 40px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  box-shadow: 0px 2px 10px rgba(0, 0, 0, 0.1);
+`;
+
+const LogoContainer = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const LogoImage = styled.img`
+  height: 50px;
+  margin-right: 15px;
+`;
+
+const Nav = styled.nav`
+  ul {
+    list-style: none;
+    display: flex;
+    margin: 0;
+    padding: 0;
+  }
+
+  li {
+    margin-left: 30px;
+  }
+
+  a {
+    text-decoration: none;
+    font-size: 16px;
+    color: #333;
+    transition: color 0.3s ease;
+
+    &:hover {
+      color: #007bff;
+    }
+  }
+`;
 
 function Header() {
   return (
-    <header className="header">
-      <div className="header-container">
-        {/* 로고 및 배너 부분 */}
-        <div className="header-logo">
-          <Link to="/">
-            <img src="/logo.jpg" alt="" className="logo-image" />
-          </Link>
-        </div>
+    <HeaderContainer>
+      <LogoContainer>
+        <Link to="/">
+          <LogoImage src="/logo.jpg" alt="" />
+        </Link>
+      </LogoContainer>
 
-        {/* 네비게이션 메뉴 */}
-        <nav className="header-nav">
-          <ul>
-            <li><Link to="/Manual">소개</Link></li>
-            <li><Link to="/GetStarted">실습하기</Link></li>
-            <li><Link to="/Developer">개발자 소개</Link></li>
-          </ul>
-        </nav>
-      </div>
-    </header>
+      <Nav>
+        <ul>
+          <li><Link to="/Manual">소개</Link></li>
+          <li><Link to="/GetStarted">실습하기</Link></li>
+          <li><Link to="/Developer">개발자 소개</Link></li>
+        </ul>
+      </Nav>
+    </HeaderContainer>
   );
 }
 
