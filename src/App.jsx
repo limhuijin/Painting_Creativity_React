8c338fef944a5027b5865ae9d86add39b59d5d28
Vactor0911 <vactor0911@gmail.com>
2024-08-27 14:31:01 +0900

    메인 요소 스타일 변경

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,10 @@ import { Header, Footer } from "./components";
 import { Home, About, GetStarted, Developers } from "./pages";
 import styled from "styled-components";
 
-const StyledRoutes = styled(Routes)`
+const Style = styled.main`
   height: auto;
   min-height: calc(100% - 4rem); // 헤더 높이
+  padding: 20px;
   padding-bottom: 4rem; // 푸터 높이
 `;
 
@@ -13,12 +14,14 @@ function App() {
   return (
     <BrowserRouter>
       <Header />
-      <StyledRoutes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/get-started" element={<GetStarted />} />
-        <Route path="/developers" element={<Developers />} />
-      </StyledRoutes>
+      <Style>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/get-started" element={<GetStarted />} />
+          <Route path="/developers" element={<Developers />} />
+        </Routes>
+      </Style>
       <Footer />
     </BrowserRouter>
   );
