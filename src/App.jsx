f23bd4d5a3692abbf7d48abb255f27e69daa400f
Vactor0911 <vactor0911@gmail.com>
2024-08-27 14:26:06 +0900

    푸터 디자인 적용

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,24 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Header, Footer } from "./components";
 import { Home, About, GetStarted, Developers } from "./pages";
+import styled from "styled-components";
+
+const StyledRoutes = styled(Routes)`
+  height: auto;
+  min-height: calc(100% - 4rem); // 헤더 높이
+  padding-bottom: 4rem; // 푸터 높이
+`;
 
 function App() {
   return (
     <BrowserRouter>
       <Header />
-      <Routes>
+      <StyledRoutes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/get-started" element={<GetStarted />} />
         <Route path="/developers" element={<Developers />} />
-      </Routes>
+      </StyledRoutes>
       <Footer />
     </BrowserRouter>
   );
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,21 @@
 import styled from "styled-components";
 
-const Style = styled.footer``;
+const Style = styled.footer`
+  width: 100%;
+  height: 4rem;
+  display: flex;
+  justify-content: space-between;
+  padding: 1rem;
+  z-index: 1000;
+  background-color: #f8f9fa;
+  position: relative;
+  transform: translateY(-100%);
+`;
 
 const Footer = () => {
   return (
     <Style>
-      <h1>Footer</h1>
+      <h4>푸터</h4>
     </Style>
   );
 };
