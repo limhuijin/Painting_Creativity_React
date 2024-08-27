7a3f131a705189ff23532a0184226b6a9a5a4928
Vactor0911 <vactor0911@gmail.com>
2024-08-27 14:02:34 +0900

    미디어 쿼리 수치 기입

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,16 @@ import styled from "styled-components";
 
 const Style = styled.div``;
 
+/*
+// 세로모드 모바일
+  @media (max-width: 991px) and (orientation: portrait) {
+  }
+
+// 가로모드 모바일
+  @media (max-width: 991px) and (orientation: landscape) {
+  }
+*/
+
 const Home = () => {
   return (
     <Style>
