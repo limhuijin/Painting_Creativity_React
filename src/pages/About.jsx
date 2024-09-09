2fc2aec9d5b1d13233d184c8a36a222202e4f1ab
Vactor0911 <vactor0911@gmail.com>
2024-09-09 12:14:48 +0900

    사용하지 않는 종속성 제거

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,6 +1,5 @@
 import styled from "styled-components";
 import { Link } from "react-router-dom";
-import { color } from "../theme";
 import Button from "../components/Button";
 
 const Style = styled.div`
