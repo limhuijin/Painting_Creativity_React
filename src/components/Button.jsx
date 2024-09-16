f22bbe6928f76742c62faf784a252bb0df7ecca9
Vactor0911 <vactor0911@gmail.com>
2024-09-16 13:03:27 +0900

    버튼 객체 리팩토링

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,20 +1,20 @@
 import styled from "styled-components";
 import { color } from "../theme";
-import PropTypes from "prop-types";
 
 const Style = styled.button`
   display: flex;
   justify-content: center;
   align-items: center;
-  padding: 16px 60px;
+  padding: 16px 70px;
   background-color: ${color.green};
   border: none;
   border-radius: 50px;
-  cursor: pointer;
-  transition: background-color 0.3s ease, transform 0.2s ease;
   box-shadow: 10px 10px 14px rgba(0, 0, 0, 0.4);
-  font-weight: bold;
-  font-size: 1.5em;
+  font-weight: 400;
+  font-size: 1.7em;
+  gap: ${props => props.$hasIcon ? "10px" : "0"};
+  transition: background-color 0.3s ease, transform 0.2s ease;
+  cursor: pointer;
 
   &:hover {
     background-color: ${color.darkGreen};
@@ -26,18 +26,13 @@ const Style = styled.button`
   }
 `;
 
-const Button = (props) => {
+const Button = ({icon, text, onClick, style, ...props}) => {
   return (
-    <Style style={{ gap: props.image ? "1em" : "0" }} onClick={props.onClick}>
-      {props.image && <img src={props.image} />}
-      {props.text}
+    <Style $hasIcon={!!icon} onClick={onClick} style={style}>
+      {icon && <img src={icon} />}
+      {text}
     </Style>
   );
-};
-Button.propTypes = {
-  image: PropTypes.string,
-  text: PropTypes.string.isRequired,
-  onClick: PropTypes.func,
 };
 
 export default Button;
